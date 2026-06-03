56c29d4d4ff8e0d79ad07454df0991d5d8618e2a
Naveen Venkatesh B P <40454790+NaveenVenkatesh23@users.noreply.github.com>
2026-06-04 02:35:41 +0530

    Update booking.blade.php

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -452,7 +452,7 @@
                 theatre_id: theatre_id,
                 addons: addonIds,
                 payment_type: paymentType,
-                coupon_code: coupon_code   
+                coupon_code: coupon_code,   
                 slot_time: document.querySelector('input[name="slot"]:checked')?.getAttribute('data-start-time') || null
             })
         })
@@ -565,7 +565,7 @@
                 body: JSON.stringify({ code: code, 
                                        amount: originalTotal, 
                                        booking_date: document.getElementById('date').value,
-                                       slot_time: document.querySelector('input[name="slot"]:checked') ? document.querySelector('input[name="slot"]:checked').getAttribute('data-start-time') : null })
+                                       slot_time: document.querySelector('input[name="slot"]:checked')?.getAttribute('data-start-time') || null })
             })
             .then(res => res.json())
             .then(data => {
